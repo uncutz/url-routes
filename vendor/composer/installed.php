bcad934142b40f65827dac47f7694840bdf44292
johannespanzer <johannes-panzer@gmx.de>
2021-10-09 19:26:25 +0200

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b813446b17910a2eee4536099d7ffd4f7b7674da',
+        'name' => 'johannespanzer/url-routes-test',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'johannespanzer/url-routes-test' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b813446b17910a2eee4536099d7ffd4f7b7674da',
+            'dev_requirement' => false,
+        ),
+    ),
+);
